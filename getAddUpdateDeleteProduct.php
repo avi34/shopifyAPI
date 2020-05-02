c1dfadef1e41e6006bac65a7d432a100f162f732
avi34 <64527129+avi34@users.noreply.github.com>
2020-05-02 23:12:37 +0530

    Update and rename get_product.php to getAddUpdateDeleteProduct.php

--- getAddUpdateDeleteProduct.php
+++ getAddUpdateDeleteProduct.php
@@ -10,7 +10,7 @@ $query = array(
 	"Content-type" => "application/json" // Tell Shopify that we're expecting a response in JSON format
 );
 
-// Run API call to get products
+// Run API call to get products list
 $products = shopify_call($token, $shop, "/admin/products.json", array(), 'GET');
 
 // Convert product JSON information into an array
@@ -27,8 +27,26 @@ $modify_data = array(
 	)
 );
 
-// Run API call to modify the product
+// Run API call to modify(update) the product
 $modified_product = shopify_call($token, $shop, "/admin/products/" . $product_id . ".json", $modify_data, 'PUT');
+
+$product_data = array(
+			"product" => array(
+				"title" => "My New Title",
+				"body_html"=> "<strong>Good snowboard!</strong>",
+				"variants"=> array(
+						"option1"=> "First",
+					        "price"=> "10.00",
+					        "sku"=>"123"
+					    ),
+				"images"=> array(
+						"src"=> "http://example.com/rails_logo.gif"
+						)       
+					)
+		);
+// echo "<pre>";print_r($product_data); exit;
+// Run API call to add the product
+$added_product = $this->shopify_call($token, $shop, "/admin/products.json", $product_data, 'POST');
 
 // Storage response
 $modified_product_response = $modified_product['response'];
